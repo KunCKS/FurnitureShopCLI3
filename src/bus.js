c7e9d037832609bb8a67393d214aac08370d73ec
Kun <freestyle5533@hotmail.com.tw>
2019-10-06 00:54:35 +0800

    v1 eslint modify

--- src/bus.js
+++ src/bus.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-Vue.prototype.$bus = new Vue();
-//在Vue實體的prototype下再新建一個名為$bus的Vue實體作為監視用
+import Vue from 'vue'
+Vue.prototype.$bus = new Vue()
+// 在Vue實體的prototype下再新建一個名為$bus的Vue實體作為監視用
 
-//自訂事件
+// 自訂事件
 
 // Alert.vue
 // 自定義事件名稱為 'messsage:push'
